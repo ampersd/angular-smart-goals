1f16c5f930214b214a09f59edc0b2d20d4a93746
Anton Korepanov <tony1000@ya.ru>
2015-11-30 01:59:11 +0500

    login template adding

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('smartApp', []);
+
+app.controller('loginController', function(){
+	this.username = "abcd";
+	this.password = "1234";
+
+	this.login = function(){
+		alert(username + " " + password);
+	};
+});
+);
