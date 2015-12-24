363c154e5bf9c2d012f8374d44bfb09e408cb25f
ampersd <tony1000@ya.ru>
2015-12-24 19:35:17 +0500

    begin implement connection to Parse

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+Parse.initialize("d1dPEa5G9BMOajwjAUOhBTXMDY5VlcaVkjX8KHZQ",
+    "qOigeKzwzvrg7kbVWPJYEbG3a9zSVft4Z0OlFXh5");
+
 var app = angular.module('smartApp', ['loginModule', 'registerModule']);
 
 app.controller('appController', function(){
+    //$rootScope.currentUser = Parse.User.current();
+
     this.view = "login";
     this.setView = function(){
         if (this.view === "login"){
--- src/auth/login.js
+++ src/auth/login.js
@@ -1,3 +1,5 @@
+
+
 var login = angular.module("loginModule", []);
 
 login.directive('loginForm', function factory(){
@@ -12,11 +14,26 @@ login.directive('loginForm', function factory(){
 
 login.controller('loginController', function(){
     this.username = "";
+    this.email = "";
     this.password = "";
 
-    this.submitForm = function(isValid){
+    this.signUp = function(isValid){
         if(isValid){
             alert(this.username + " " + this.password);
+
+            // TODO: if I understand right for properly user creating in parse we need to add email field too I think it's ok for creating
+            // what about authorization there?
+            var user = new Parse.User();
+            user.set("username", this.username);
+            user.set("password", this.password);
+            var result = user.signUp(null, {
+                success: function(user) {
+                    return user;
+                },
+                error: function(user, error) {
+                    alert("Unable to sign up: " + error.code + " " + error.message);
+                }
+            });
         }
     };
 });
